86f4f9d9cd8c306fcd0f851394bb3f1f354b04aa
Notenlish <71970100+Notenlish@users.noreply.github.com>
2025-05-03 18:55:33 +0300

    caching

--- anifetch.py
+++ anifetch.py
@@ -3,6 +3,9 @@ import os
 import pathlib
 import argparse
 import shutil
+import time
+
+st = time.time()
 
 GAP = 2
 PAD_LEFT = 4
@@ -89,7 +92,7 @@ HEIGHT = args.height
 # put cached frames here
 frames: list[str] = []
 
-# Generate ASCII Video Frames via chafa & ffmpeg
+# cache is invalid, re-render
 if should_update:
     print_verbose("SHOULD RENDER WITH CHAFA")
 
@@ -113,40 +116,49 @@ if should_update:
         stderr=stderr,
     )
     
-    
-    # TODO: I think I'm also supposed to remove the output cache(in case the number of animation frames of the new animation are shorter than the old animation)
-    # if the new anim frames is shorter than the old one, then in /output there will be both new and old frames.
+    # If the new anim frames is shorter than the old one, then in /output there will be both new and old frames. Empty the directory to fix this.
     shutil.rmtree("/tmp/anifetch/output")
     os.mkdir("/tmp/anifetch/output")
-
-
-
-# get the frames
-animation_files = os.listdir(pathlib.Path("/tmp/anifetch/video"))
-animation_files.sort()
-for i, f in enumerate(animation_files):
-    print_verbose(f"STARTING WITH FRAME {f}")
     
-    # f = 001.png
-    path = pathlib.Path("/tmp/anifetch/video") / f
-    chafa_cmd = [
-        "chafa",
-        *args.chafa_arguments.split(" "),
-        # "--color-space=rgb",
-        f"--size={WIDTH}x{HEIGHT}",
-        path.as_posix(),
-    ]
-    # print("CHAFA", " ".join(chafa_cmd))
-    frame = subprocess.check_output(
-        chafa_cmd,
-        text=True,
-    )
+    print_verbose("Emptied the output folder.")
 
-    # if wanted aspect ratio doesnt match source, chafa makes width as high as it can, and adjusts height accordingly.
-    # AKA: even if I specify 40x20, chafa might give me 40x11 or something like that.
-    if i == 0:
-        HEIGHT = len(frame.splitlines())
-    frames.append(frame)
+    # get the frames
+    animation_files = os.listdir(pathlib.Path("/tmp/anifetch/video"))
+    animation_files.sort()
+    for i, f in enumerate(animation_files):
+        print_verbose(f"- Frame: {f}")
+        
+        # f = 00001.png
+        path = pathlib.Path("/tmp/anifetch/video") / f
+        chafa_cmd = [
+            "chafa",
+            *args.chafa_arguments.split(" "),
+            # "--color-space=rgb",
+            f"--size={WIDTH}x{HEIGHT}",
+            path.as_posix(),
+        ]
+        # print("CHAFA", " ".join(chafa_cmd))
+        frame = subprocess.check_output(
+            chafa_cmd,
+            text=True,
+        )
+        
+        with open((pathlib.Path("/tmp/anifetch/output") / f).with_suffix(".txt"), "w") as file:
+            file.write(frame)
+        
+        # if wanted aspect ratio doesnt match source, chafa makes width as high as it can, and adjusts height accordingly.
+        # AKA: even if I specify 40x20, chafa might give me 40x11 or something like that.
+        if i == 0:
+            HEIGHT = len(frame.splitlines())
+        frames.append(frame)
+else:
+    # just use cached
+    for filename in os.listdir("/tmp/anifetch/output"):
+        path = pathlib.Path("/tmp/anifetch/output") / filename
+        with open(path, "r") as file:
+            frame = file.read()
+            frames.append(frame)
+    HEIGHT = len(frames[0].splitlines())
 
 print_verbose("-----------")
 
@@ -160,15 +172,10 @@ for y, neo_line in enumerate(neo_output):
     except IndexError:
         chafa_line = ""
 
-    width_to_offset = GAP
+    width_to_offset = GAP + WIDTH
 
-    if chafa_line:
-        if y == HEIGHT - 1:  # get rid of the empty end frame
-            width_to_offset = GAP + WIDTH
-    if not chafa_line:
-        width_to_offset = GAP + WIDTH
-
-    output = f"{(PAD_LEFT+(GAP*2)) * " "}{' ' * width_to_offset}{neo_line}\n"
+    # I have no idea why this is the way it is.
+    output = f"{(PAD_LEFT+ (GAP*2) ) * " "}{' ' * width_to_offset}{neo_line}\n"
     template.append(output)
 
 # writing the tempate to a file.
@@ -177,6 +184,7 @@ with open("/tmp/anifetch/template.txt", "w") as f:
     # I just need to move this down, and also apply that padding thingy(for lines that dont have chafa anim)
     # so basically repeat what I have done but this time its for layout.
     # If I do this then I can get rid of the layout padding code on the last part. because the layout will already be fixed.
+print_verbose("Template updated")
 
 # for defining the positions of the cursor, that way I can set cursor pos and only redraw a portion of the text, not the entire text.
 TOP = 2
@@ -184,53 +192,13 @@ LEFT = PAD_LEFT
 RIGHT = WIDTH + PAD_LEFT
 BOTTOM = HEIGHT# + TOP
 
-# assuming that the neofetch text is longer vertically
-out_frames: list[str] = []
-for frame_i, frame in enumerate(frames):
-    print_verbose(f"PROCESSING frame {frame_i}")
-    width_to_offset = 0
-    chafa_rows = frame.splitlines()
-
-    out_frame_arr = []
-    for y, neo_line in enumerate(neo_output):
-        output = ""
-        try:
-            chafa_line = chafa_rows[y]
-        except IndexError:
-            chafa_line = ""
-
-        width_to_offset = GAP
-
-        if chafa_line:
-            if y == HEIGHT - 1:  # get rid of the empty end frame
-                width_to_offset = GAP + WIDTH
-        if not chafa_line:
-            width_to_offset = GAP + WIDTH #+ PAD_LEFT
-
-        output = f"{PAD_LEFT * " "}{chafa_line}{' ' * width_to_offset}{neo_line}"
-
-        out_frame_arr.append(output)
-
-    out_frame: str = "\n".join(out_frame_arr)
-    out_frames.append(out_frame)
-
-print_verbose("FINISHED PROCESSING.")
-
-
-print_verbose("First Frame Preview: ")
-print_verbose(out_frames[0])
-
-for i, frame in enumerate(out_frames):
-    with open(f"/tmp/anifetch/output/{i}.txt", "w") as file:
-        file.write(frame)
-
-print_verbose("WROTE TO /tmp/anifetch/output/ .")
-
 script_dir = os.path.dirname(__file__)
 script_path = os.path.join(script_dir, "loop-cursor.sh")
 
 # print(args.framerate, TOP, LEFT, RIGHT, BOTTOM)
 # raise SystemExit
+
+print(f"It took {time.time() - st} seconds.")
 
 try:
     subprocess.call(["bash", script_path, str(args.framerate), str(TOP), str(LEFT), str(RIGHT), str(BOTTOM)], text=True)
